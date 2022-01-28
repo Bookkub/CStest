4aa27a62e64f1633b8fb7c3ec6d261b85bf280d7
Bookkub <kittiboo68@gmail.com>
2022-01-28 18:47:32 +0700

    Hello

---
+++ SimpleClient.java
@@ -0,0 +1,27 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+public class SimpleClient{
+    public static void main(String args[]) throws IOException{
+        Socket s1 = new Socket("localhost",1234);
+        OutputStream s1out = s1.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(s1out);
+        System.out.print("Input your word : ");
+        Scanner word_1 = new Scanner(System.in);
+        String w_1 = word_1.nextLine();
+        dos.writeUTF(w_1);
+        dos.flush();
+        System.out.print("What word do you want to find? : ");
+        Scanner word_2 = new Scanner(System.in);
+        String w_2 = word_2.nextLine();
+        dos.writeUTF(w_2);
+        InputStream s1In = s1.getInputStream();
+        DataInputStream dis = new DataInputStream(s1In);
+        String sen_1 = new String(dis.readUTF());
+        System.out.print(sen_1);
+        // dis.readUTF(sen_1);
+        dos.close();
+        s1out.close();
+        s1.close();
+    }
+}
---
+++ SimpleServer.java
@@ -0,0 +1,57 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class SimpleServer {
+    public static void main(String args[]) throws IOException {
+        ServerSocket s = new ServerSocket(1234);
+        Socket s1 = s.accept();
+        InputStream s1In = s1.getInputStream();
+        DataInputStream dis = new DataInputStream(s1In);
+        String word_1 = new String(dis.readUTF());
+        String word_2 = new String(dis.readUTF());
+        System.out.println(word_1);
+        System.out.println(word_2);
+        int i, k=0, flag = 0, len_1 = 0, len_2 = 0;
+        len_1 = word_1.length();
+        len_2 = word_2.length();
+        word_1 = word_1.toLowerCase();
+        word_2 = word_2.toLowerCase();
+        char[] ch_1 = word_1.toCharArray();
+        char[] ch_2 = word_2.toCharArray();
+        if(len_1<len_2)
+        {
+            flag = 2;
+        }
+        else
+        {
+            for (i = 0; i < len_1; i++) 
+            {
+                if (ch_1[i] == ch_2[k]) 
+                {
+                    k++;
+                    if (k == len_2) {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        OutputStream s1out = s1.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(s1out);
+        if (flag == 1) {
+            System.out.println("These are Kangaroo words.");
+            dos.writeUTF("These are Kangaroo words.");
+        } else if (flag == 2) {
+            System.out.println("Finding words character less than main words.");
+            dos.writeUTF("Finding words character less than main words.");
+        } else {
+            System.out.println("These are not Kangaroo words.");
+            dos.writeUTF("These are not Kangaroo words.");
+        }
+        dis.close();
+        s1In.close();
+        s1.close();
+    }
+}
