102b21f3f24632531602af5249e45ac34f1bb69c
Bookkub <kittiboo68@gmail.com>
2022-03-25 15:28:38 +0700

    h

--- SimpleClient.java
+++ SimpleClient.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.Scanner;
 public class SimpleClient{
     public static void main(String args[]) throws IOException{
-        Socket s1 = new Socket("192.168.1.7",1234);
+        Socket s1 = new Socket("172.19.224.1",1234);
         OutputStream s1out = s1.getOutputStream();
         DataOutputStream dos = new DataOutputStream(s1out);
         System.out.print("Input your word : ");
